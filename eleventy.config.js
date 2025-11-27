b3ff1a5dfb296ca129664c587a1d279181e3ac60
Josh Goodman <jogoodman@gmail.com>
2025-11-26 19:45:59 -0500

    Add WFIU show recording

--- eleventy.config.js
+++ eleventy.config.js
@@ -81,6 +81,7 @@ export default function (eleventyConfig) {
 
   // Compile Tailwind before Eleventy processes the files.
   eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/assets/audio");
   eleventyConfig.on("eleventy.before", async () => {
     const tailwindInputPath = path.resolve("./src/assets/styles/index.css");
 
